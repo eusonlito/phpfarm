e1fa33c96e9c1a896fdf5d83ad3bace5fba5aa88
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-08-14 14:22:36 +0300

    added sample script to build extensions

---
+++ build_ext/install_ext_memcache.sh
@@ -0,0 +1,11 @@
+(
+	mkdir -p ./src/
+	cd ./src/
+	pecl download memcache
+	tar -xvzf ./memcache-*.tgz
+	cd ./memcache-*
+
+	(phpize-5.3.28 && ./configure --with-php-config=/opt/phpfarm/inst/bin/php-config-5.3.28 && make && make install && make clean)
+	(phpize-5.4.31 && ./configure --with-php-config=/opt/phpfarm/inst/bin/php-config-5.4.31 && make && make install && make clean)
+	(phpize-5.6.0RC3 && ./configure --with-php-config=/opt/phpfarm/inst/bin/php-config-5.6.0RC3 && make && make install && make clean)
+)
