950d56f6dae90cafecfcb1c20292f2e293861560
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-19 11:36:13 +0300

    updated to new versions

--- build_ext/install_ext_memcache.sh
+++ build_ext/install_ext_memcache.sh
@@ -5,7 +5,10 @@
 	tar -xvzf ./memcache-*.tgz
 	cd ./memcache-*
 
-	(phpize-5.3.28 && ./configure --with-php-config=/opt/phpfarm/inst/bin/php-config-5.3.28 && make && make install && make clean)
-	(phpize-5.4.31 && ./configure --with-php-config=/opt/phpfarm/inst/bin/php-config-5.4.31 && make && make install && make clean)
-	(phpize-5.6.0RC3 && ./configure --with-php-config=/opt/phpfarm/inst/bin/php-config-5.6.0RC3 && make && make install && make clean)
+#	(phpize-5.3.28 && ./configure --with-php-config=/opt/phpfarm/inst/bin/php-config-5.3.28 && make && make install && make clean)
+	(phpize-5.3.29 && ./configure --with-php-config=/opt/phpfarm/inst/bin/php-config-5.3.29 && make && make install && make clean)
+#	(phpize-5.4.31 && ./configure --with-php-config=/opt/phpfarm/inst/bin/php-config-5.4.31 && make && make install && make clean)
+	(phpize-5.4.32 && ./configure --with-php-config=/opt/phpfarm/inst/bin/php-config-5.4.32 && make && make install && make clean)
+#	(phpize-5.6.0RC3 && ./configure --with-php-config=/opt/phpfarm/inst/bin/php-config-5.6.0RC3 && make && make install && make clean)
+	(phpize-5.6.0 && ./configure --with-php-config=/opt/phpfarm/inst/bin/php-config-5.6.0 && make && make install && make clean)
 )
