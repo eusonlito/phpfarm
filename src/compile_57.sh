bd526790f87f5f4082c0475e160941d4f62e6405
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-10-28 15:27:38 +0200

    5.7.0-dev compile fix

--- src/compile_57.sh
+++ src/compile_57.sh
@@ -10,6 +10,15 @@ git clone https://git.php.net/repository/php-src.git --depth 1 --branch phpng --
 #git checkout phpng
 #git pull
 ./buildconf
+
+# Compiling PHP with Bison 2.7+
+#sed -i -r 's/(bison_version_list=.*)"/\1 2.7 3.0"/' configure
+# Compiling PHP with Bison x.y.any.version.scheme.your.distro.want
+#sed -i -r "s;(bison_version_vars=.*)/ /'(.*);\1/ /g\2;" configure
+
+#bison_version_exclude="3.0"
+sed -i -r 's/(bison_version_exclude=").*"/\1 none"/' configure
+
 )
 
 ./compile.sh $ver
